788d4b424ba9c61b9d6dfec56293a925fbf7b0f3
Jack Sierkstra <jack.sierkstra@ing.nl>
2018-11-14 14:02:28 +0100

    Initial commit.

---
+++ src/main/java/com/example/bookstorespring/BookstoreSpringApplication.java
@@ -0,0 +1,12 @@
+package com.example.bookstorespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreSpringApplication.class, args);
+	}
+}
